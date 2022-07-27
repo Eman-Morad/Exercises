56578e46b972b53206e00232865aeb9e8a5d4836
Eman-Morad <emanadel.104@gmail.com>
2022-07-27 11:28:50 +0100

    Adding output for multiples of 7

--- step1.py
+++ step1.py
@@ -12,13 +12,20 @@ def Multiple_of_five(num):
   else:
     return""
 
+def Multiple_of_seven(num):
+  # Function returns Bang if the input is a numper multiple of seven.
+  if(num % 7) == 0:
+    return "Bang"
+  else:
+    return"" 
 
 for x in range(1,101):
 
-  mo3_text = Multiple_of_three(x)
-  mo5_text = Multiple_of_five(x)
-
-  if mo3_text or mo5_text:
-    print(mo3_text + mo5_text)
+  mo3_text  = Multiple_of_three(x)
+  mo5_text  = Multiple_of_five(x)
+  mo7_text  = Multiple_of_seven(x)
+  
+  if mo3_text or mo5_text or mo7_text :
+    print(mo3_text + mo5_text + mo7_text)
   else:
     print(x)

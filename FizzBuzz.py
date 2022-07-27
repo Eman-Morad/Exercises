79da6682f58ae5530be4a8b533b8f5cb6d4f8050
Eman-Morad <emanadel.104@gmail.com>
2022-07-27 12:18:34 +0100

    Adding output for multiples of 17

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -32,6 +32,14 @@ def Multiple_of_thirteen(num):
     return "Fezz"
   else:
     return"" 
+
+def Multiple_of_seventeen(num):
+  # Function returns True if the input is a numper multiple of seventeen.
+  if(num % 17) == 0:
+    return True
+  else:
+    return False
+
     
 for x in range(1,300):
 
@@ -40,12 +48,25 @@ for x in range(1,300):
   mo7_text  = Multiple_of_seven(x)
   mo11_text = Multiple_of_eleven(x)
   mo13_text = Multiple_of_thirteen(x)
+  mo17_check = Multiple_of_seventeen(x)
   
   if mo11_text :
+    text_list = [mo13_text, mo11_text]
     # Prints Bong if mutiplies by 11.
     # Prints FezzBong if multiplies by 11 and 13.
-    print(mo13_text + mo11_text)      
+    if mo17_check:
+      # Reverse the output of multiple of 17.
+      text_list = reversed(text_list)
+    
+    print(''.join(text_list))
+       
   elif mo3_text or mo5_text or mo7_text or mo13_text:
-    print(mo3_text + mo13_text + mo5_text + mo7_text)
+    text_list = [mo3_text, mo13_text, mo5_text, mo7_text]
+
+    if mo17_check:
+      text_list = reversed(text_list)
+
+    print(''.join(text_list))
+  
   else:
     print(x)

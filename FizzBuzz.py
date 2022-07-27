cbefebd30cdb6ca52a56302858aa69bb2f3dd5b7
Eman-Morad <emanadel.104@gmail.com>
2022-07-27 11:45:32 +0100

    Adding output for multiples of 13

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -26,16 +26,26 @@ def Multiple_of_eleven(num):
   else:
     return"" 
 
-for x in range(1,101):
+def Multiple_of_thirteen(num):
+  # Function returns Fezz if the input is a numper multiple of thirteen.
+  if(num % 13) == 0:
+    return "Fezz"
+  else:
+    return"" 
+    
+for x in range(1,300):
 
   mo3_text  = Multiple_of_three(x)
   mo5_text  = Multiple_of_five(x)
   mo7_text  = Multiple_of_seven(x)
   mo11_text = Multiple_of_eleven(x)
+  mo13_text = Multiple_of_thirteen(x)
   
   if mo11_text :
-    print(mo11_text)  
-  elif mo3_text or mo5_text or mo7_text :
-    print(mo3_text + mo5_text + mo7_text)
+    # Prints Bong if mutiplies by 11.
+    # Prints FezzBong if multiplies by 11 and 13.
+    print(mo13_text + mo11_text)      
+  elif mo3_text or mo5_text or mo7_text or mo13_text:
+    print(mo3_text + mo13_text + mo5_text + mo7_text)
   else:
     print(x)
